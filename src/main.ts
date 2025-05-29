c4ef6bffac069b7d369065ea1378a64e7a1fc738
chicoumbetov <shyngysmaaan@gmail.com>
2025-05-29 12:13:26 +0200

    :recycle: use corsOptions

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,15 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 import * as cookieParser from 'cookie-parser'
+import corsOptions from './config/corsOptions'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
 	app.use(cookieParser())
-	app.enableCors({
-		origin: [process.env.CLIENT_URL],
-		credentials: true,
-		exposedHeaders: 'set-cookie'
-	})
 
-	await app.listen(process.env.PORT ?? 5000)
+	app.enableCors(corsOptions)
+
+	await app.listen(process.env.PORT ?? 5001)
 }
 bootstrap()
