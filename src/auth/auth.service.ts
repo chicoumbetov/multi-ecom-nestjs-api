3fb60abcc892088a1bec302bc594923495a93fab
chicoumbetov <shyngysmaaan@gmail.com>
2025-05-30 22:58:43 +0200

    refactor cookie

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -114,8 +114,8 @@ export class AuthService {
 			httpOnly: true,
 			domain: this.configService.get('SERVER_DOMAIN'),
 			expires: expiresIn,
-			secure: process.env.NODE_ENV === 'production',
-			sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+			secure: process.env.NODE_ENV === 'prod',
+			sameSite: process.env.NODE_ENV === 'prod' ? 'none' : 'lax'
 		})
 	}
 
