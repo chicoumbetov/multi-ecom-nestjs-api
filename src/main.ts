6a28133708012ff2ab6cecd6de9f2a80f7e3df56
chicoumbetov <shyngysmaaan@gmail.com>
2025-05-30 22:58:33 +0200

    refactor swagger titles

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,10 @@ async function bootstrap() {
 	app.enableCors(corsOptions)
 
 	const config = new DocumentBuilder()
-		.setTitle('Cats example')
-		.setDescription('The cats API description')
+		.setTitle('Multi E - com shops Nest JS Api')
+		.setDescription('The API description')
 		.setVersion('1.0')
-		.addTag('cats')
+		.addTag('multi-e-com')
 		.build()
 	const documentFactory = () => SwaggerModule.createDocument(app, config)
 	SwaggerModule.setup('api', app, documentFactory)
