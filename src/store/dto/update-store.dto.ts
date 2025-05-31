473807e58f543541817d31203ae02da58bc66f4b
chicoumbetov <shyngysmaaan@gmail.com>
2025-05-31 11:21:23 +0200

    :recycle: UpdateStoreDto

--- src/store/dto/update-store.dto.ts
+++ src/store/dto/update-store.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateStoreDto } from './create-store.dto';
+import { PartialType } from '@nestjs/swagger'
+import { IsString } from 'class-validator'
+import { CreateStoreDto } from './create-store.dto'
 
-export class UpdateStoreDto extends PartialType(CreateStoreDto) {}
+export class UpdateStoreDto extends PartialType(CreateStoreDto) {
+	@IsString({
+		message: 'description-required'
+	})
+	description: string
+}
