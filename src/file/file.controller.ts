89a37b6dc8aca279634688e01ae8f367502dddf0
chicoumbetov <shyngysmaaan@gmail.com>
2025-05-31 14:04:59 +0200

    :recycle: file service controller module

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,34 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { FileService } from './file.service';
-import { CreateFileDto } from './dto/create-file.dto';
-import { UpdateFileDto } from './dto/update-file.dto';
+import {
+	Controller,
+	HttpCode,
+	Post,
+	Query,
+	UploadedFiles,
+	UseInterceptors
+} from '@nestjs/common'
+import { FilesInterceptor } from '@nestjs/platform-express'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { FileService } from './file.service'
 
-@Controller('file')
+@Controller('files')
 export class FileController {
-  constructor(private readonly fileService: FileService) {}
+	constructor(private readonly fileService: FileService) {}
 
-  @Post()
-  create(@Body() createFileDto: CreateFileDto) {
-    return this.fileService.create(createFileDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.fileService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.fileService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
-    return this.fileService.update(+id, updateFileDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.fileService.remove(+id);
-  }
+	@HttpCode(200)
+	@UseInterceptors(FilesInterceptor('files'))
+	@Auth()
+	@Post()
+	async saveFiles(
+		@UploadedFiles() files: Express.Multer.File[],
+		@Query('folder') folder?: string
+	) {
+		return this.fileService.saveFiles(files, folder)
+	}
 }
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
-import { CreateFileDto } from './dto/create-file.dto';
-import { UpdateFileDto } from './dto/update-file.dto';
+import { Injectable } from '@nestjs/common'
+import { path } from 'app-root-path'
+import { ensureDir, writeFile } from 'fs-extra'
+import { CreateFileDto } from './dto/create-file.dto'
+import { UpdateFileDto } from './dto/update-file.dto'
+import { FileResponse } from './file.interface'
 
 @Injectable()
 export class FileService {
-  create(createFileDto: CreateFileDto) {
-    return 'This action adds a new file';
-  }
+	async saveFiles(files: Express.Multer.File[], folder: string = 'products') {
+		const uploadedFolder = `${path}/uploads/${folder}`
 
-  findAll() {
-    return `This action returns all file`;
-  }
+		await ensureDir(uploadedFolder)
 
-  findOne(id: number) {
-    return `This action returns a #${id} file`;
-  }
+		const response: FileResponse[] = await Promise.all(
+			files.map(async file => {
+				const originalName = `${Date.now()}-${file.originalname}`
 
-  update(id: number, updateFileDto: UpdateFileDto) {
-    return `This action updates a #${id} file`;
-  }
+				await writeFile(
+					`${uploadedFolder}/${originalName}`,
+					file.buffer
+				)
 
-  remove(id: number) {
-    return `This action removes a #${id} file`;
-  }
+				return {
+					url: `/uploads/${folder}/${originalName}`,
+					name: originalName
+				}
+			})
+		)
+
+		return response
+	}
+
+	create(createFileDto: CreateFileDto) {
+		return 'This action adds a new file'
+	}
+
+	findAll() {
+		return `This action returns all file`
+	}
+
+	findOne(id: number) {
+		return `This action returns a #${id} file`
+	}
+
+	update(id: number, updateFileDto: UpdateFileDto) {
+		return `This action updates a #${id} file`
+	}
+
+	remove(id: number) {
+		return `This action removes a #${id} file`
+	}
 }
