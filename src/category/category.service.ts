3d4a485380a253067b0c5ee7c232b983a7fe5f12
chicoumbetov <shyngysmaaan@gmail.com>
2025-05-31 13:45:46 +0200

    :sparkles: CategoryService

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,26 +1,63 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { CategoryDto } from './dto/category.dto'
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
-  }
+	constructor(private prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all category`;
-  }
+	async getByStoreId(storeId: string) {
+		return this.prisma.category.findMany({
+			where: {
+				storeId
+			}
+		})
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
-  }
+	async getById(id: string) {
+		const category = await this.prisma.category.findUnique({
+			where: {
+				id
+			}
+		})
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
-  }
+		if (!category) throw new NotFoundException('category-not-found')
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
-  }
+		return category
+	}
+
+	async create(storeId: string, dto: CategoryDto) {
+		return this.prisma.category.create({
+			data: {
+				title: dto.title,
+				description: dto.description,
+				storeId
+			}
+		})
+	}
+
+	async update(id: string, dto: CategoryDto) {
+		await this.getById(id)
+
+		return this.prisma.category.update({
+			where: { id },
+			data: dto
+		})
+	}
+
+	async delete(id: string) {
+		await this.getById(id)
+
+		return this.prisma.category.delete({
+			where: { id }
+		})
+	}
+
+	findAll() {
+		return `This action returns all category`
+	}
+
+	findOne(id: number) {
+		return `This action returns a #${id} category`
+	}
 }
