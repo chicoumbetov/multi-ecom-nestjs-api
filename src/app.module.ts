b7b861aade83821389cc8333ca51ca651fe446d6
chicoumbetov <shyngysmaaan@gmail.com>
2025-05-31 22:42:54 +0200

    :sparkles: use all module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,30 @@ import { Module } from '@nestjs/common'
 
 import { AuthModule } from './auth/auth.module'
 import { JwtStrategy } from './auth/strategies/jwt.strategy'
+import { CategoryModule } from './category/category.module'
+import { ColorModule } from './color/color.module'
 import { AppConfigModule } from './config/config.module'
+import { FileModule } from './file/file.module'
+import { ReviewModule } from './review/review.module'
+import { StoreModule } from './store/store.module'
 import { UserModule } from './user/user.module'
-import { StoreModule } from './store/store.module';
-import { ColorModule } from './color/color.module';
-import { ReviewModule } from './review/review.module';
-import { CategoryModule } from './category/category.module';
-import { FileModule } from './file/file.module';
-import { ProductModule } from './product/product.module';
-import { OrderModule } from './order/order.module';
+
+import { OrderModule } from './order/order.module'
+import { ProductModule } from './product/product.module'
 
 @Module({
-	imports: [AppConfigModule, AuthModule, UserModule, StoreModule, ColorModule, ReviewModule, CategoryModule, FileModule, ProductModule, OrderModule],
+	imports: [
+		AppConfigModule,
+		AuthModule,
+		UserModule,
+		StoreModule,
+		ColorModule,
+		ReviewModule,
+		CategoryModule,
+		FileModule,
+		ProductModule,
+		OrderModule
+	],
 	providers: [JwtStrategy]
 })
 export class AppModule {}
