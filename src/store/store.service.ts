9ddde94c302e0a48530deeaab9b84bd37dedbd93
chicoumbetov <shyngysmaaan@gmail.com>
2025-05-31 11:21:38 +0200

    :sparkles: StoreService

--- src/store/store.service.ts
+++ src/store/store.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
-import { CreateStoreDto } from './dto/create-store.dto';
-import { UpdateStoreDto } from './dto/update-store.dto';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { CreateStoreDto } from './dto/create-store.dto'
+import { UpdateStoreDto } from './dto/update-store.dto'
 
 @Injectable()
 export class StoreService {
-  create(createStoreDto: CreateStoreDto) {
-    return 'This action adds a new store';
-  }
+	constructor(private prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all store`;
-  }
+	async getById(storeId: string, userId: string) {
+		const store = await this.prisma.store.findUnique({
+			where: {
+				id: storeId,
+				userId
+			}
+		})
 
-  findOne(id: number) {
-    return `This action returns a #${id} store`;
-  }
+		if (!store)
+			throw new NotFoundException('shop-not-found-or-you-are-not-owner')
 
-  update(id: number, updateStoreDto: UpdateStoreDto) {
-    return `This action updates a #${id} store`;
-  }
+		return store
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} store`;
-  }
+	async create(userId: string, dto: CreateStoreDto) {
+		return this.prisma.store.create({
+			data: {
+				title: dto.title,
+				userId
+			}
+		})
+	}
+
+	async update(storeId: string, userId: string, dto: UpdateStoreDto) {
+		await this.getById(storeId, userId)
+
+		return this.prisma.store.update({
+			where: { id: storeId },
+			data: {
+				...dto,
+				userId
+			}
+		})
+	}
+
+	async delete(storeId: string, userId: string) {
+		await this.getById(storeId, userId)
+
+		return this.prisma.store.delete({
+			where: { id: storeId }
+		})
+	}
+
+	findAll() {
+		return `This action returns all store`
+	}
+
+	findOne(id: number) {
+		return `This action returns a #${id} store`
+	}
 }
